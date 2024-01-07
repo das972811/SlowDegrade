9439f4b15abd3fc38cf2deba9ecb181f93e81503
das972811 <salasdiego54@gmail.com>
2024-01-06 22:59:44 -0600

    Added a base layout componenet

---
+++ src/components/shared/BaseLayout.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <slot name="header"></slot>
+    <slot></slot>
+    <slot name="footer"></slot>
+</template>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,7 +1,22 @@
 <script setup>
+    import BaseLayout from '@/components/shared/BaseLayout.vue';
     import BaseBackgroundVideo from '@/components/shared/BaseBackgroundVideo.vue';
     import BackgroundVideo from '@/assets/video/landingPageVideoBG.mp4';
 </script>
 <template>
-    <base-background-video :src=BackgroundVideo ></base-background-video>
+    <base-layout>
+        <template #header>
+            <header>
+
+            </header>
+        </template>
+        <template #default>
+            <base-background-video :src=BackgroundVideo ></base-background-video>
+        </template>
+        <template #footer>
+            <footer>
+                
+            </footer>
+        </template>
+    </base-layout>
 </template>
